5ac362235ff998713d72a1d295efe0787064fc5d
Nekasu <zxt1428147954@163.com>
2024-11-12 21:38:18 +0800

    新增一个增强掩膜的函数，该函数将掩膜二值化,现在掩膜中的内容非黑即白

--- utils/binarify_mask.py
+++ utils/binarify_mask.py
@@ -10,5 +10,7 @@ def binarify_mask(in_image_path_name, out_image_path_name):
             r, g, b = pixels[x, y]
             if (r <=10) and (g<=10) and (b<=10):
                 pixels[x,y] = (0, 0, 0)
+            else:
+                pixels[x, y] = (255, 255, 255)
     
     image.save(out_image_path_name)
